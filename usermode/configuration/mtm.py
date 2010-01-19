3f0390109f200d1ab59dd8f1e953d7386e82d306
Andres Jaan Tack <ajtack@gmail.com>
2010-01-18 18:59:17 -0600

    Reorganized SConscripts to prioritize those things which must be legible.
    Re-employed some pretty build output. The VERBOSE flag turns this off.
    Removed lame old comments.
    Established one build environment (mtm.Environment) for the project.

--- usermode/configuration/mtm.py
+++ usermode/configuration/mtm.py
@@ -11,67 +11,6 @@ class Environment(SCons.Environment.Environment):
 		munging of the build variables needed in the source files.
 	"""
 	
-	#: Build options which are either on or off.
-	_boolean_options = {
-		'ROLLOVER_CLOCK':           ('Roll over clock when it reaches its maximum value.  Clock rollover can be safely disabled on 64 bits to save a few cycles, but it is necessary on 32 bits if the application executes more than 2^28 (write-through) or 2^31 (write-back) transactions.',
-			True),
-		'CLOCK_IN_CACHE_LINE':      ('Ensure that the global clock does not share the same cache line than some other variable of the program.  This should be normally enabled.',
-			True),
-		'NO_DUPLICATES_IN_RW_SETS': ('Prevent duplicate entries in read/write sets when accessing the same address multiple times.  Enabling this option may reduce performance so leave it disabled unless transactions repeatedly read or write the same address.',
-			True),
-		'WAIT_YIELD':               ('Yield the processor when waiting for a contended lock to be released. This only applies to the CM_WAIT and CM_PRIORITY contention managers.',
-			True),
-		'EPOCH_GC':                 ('Use an epoch-based memory allocator and garbage collector to ensure that accesses to the dynamic memory allocated by a transaction from another transaction are valid.  There is a slight overhead from enabling this feature.',
-			True),
-		'CONFLICT_TRACKING':        ('Keep track of conflicts between transactions and notifies the application (using a callback), passing the identity of the two conflicting transaction and the associated threads.  This feature requires EPOCH_GC.',
-			True),
-		'READ_LOCKED_DATA':         ('Allow transactions to read the previous version of locked memory locations, as in the original LSA algorithm (see [DISC-06]). This is achieved by peeking into the write set of the transaction that owns the lock.  There is a small overhead with non-contended workloads but it may significantly reduce the abort rate, especially with transactions that read much data.  This feature only works with the WRITE_BACK_ETL design and requires EPOCH_GC.',
-			True),
-		'LOCK_IDX_SWAP':            ('Tweak the hash function that maps addresses to locks so that consecutive addresses do not map to consecutive locks. This can avoid cache line invalidations for application that perform sequential memory accesses. The last byte of the lock index is swapped with the previous byte.',
-			True),
-		
-		# These are more to do with build behavior and output
-		'VERBOSE': ('If set, displays the actual commands used and their flags instead of the default "neat" output.',
-			False)
-	}
-	
-	#: Build options which have enumerated values.
-	_enumerable_options = [
-		EnumVariable('CONFLICT_MANAGER',
-		                 'Determines the conflict_management policy for the STM.',
-		                 'CM_SUICIDE',
-		                 ['CM_SUICIDE', 'CM_DELAY', 'CM_BACKOFF', 'CM_PRIORITY'])
-	]
-	
-	#: Build options which have numerical values
-	_numerical_options = [
-		('RW_SET_SIZE',
-		 'Initial size of the read and write sets. These sets will grow dynamically when they become full.',
-		 4096 # Default
-				 ),
-		('LOCK_ARRAY_LOG_SIZE',
-		 'Number of bits used for indexes in the lock array. The size of the array will be 2 to the power of LOCK_ARRAY_LOG_SIZE.',
-		 20 # Default
-				 ),
-		('MIN_BACKOFF',
-		 'Minimum value of the exponential backoff delay. This parameter is only used with the CM_BACKOFF contention manager.',
-		 0x4 # Default
-		 ),
-		('MAX_BACKOFF',
-		 'Maximum value of the exponential backoff delay. This parameter is only used with the CM_BACKOFF contention manager.',
-		 0x80000000 # Default
-		 ),
-		('VR_THRESHOLD_DEFAULT', 
-		 'Number of aborts due to failed validation before switching to visible reads. A value of 0 indicates no limit. This parameter is only used with the CM_PRIORITY contention manager. It can also be set using an environment variable of the same name.',
-		 3 # Default
-		 ),
-		('CM_THRESHOLD_DEFAULT',
-		 'Number of executions of the transaction with a CM_SUICIDE contention management strategy before switching to CM_PRIORITY. This parameter is only used with the CM_PRIORITY contention manager. It can also be set using an environment variable of the same name.',
-		 0 # Default
-		 )
-		 
-	]
-	
 	def __init__(self, configuration_name):
 		"""
 			Applies the definitions in configuration_name to generate a correct
@@ -100,11 +39,13 @@ class Environment(SCons.Environment.Environment):
 			LIBS = ['atomic_ops'])
 		
 		# Make output pretty.
-		#if not self['VERBOSE']:
-	#		self.Replace(
-	#			    CCCOMSTR = '(COMPILE) $SOURCES',
-	#			    ARCOMSTR = '(BUILD)   $TARGET',
-	#			RANLIBCOMSTR = '(INDEX)   $TARGET')
+		if not self['VERBOSE']:
+			self.Replace(
+				    CCCOMSTR = '(COMPILE) $SOURCES',
+				    ARCOMSTR = '(BUILD)   $TARGET',
+				RANLIBCOMSTR = '(INDEX)   $TARGET',
+				  SHCCCOMSTR = '(COMPILE) $SOURCES',
+				  LINKCOMSTR = '(LINK)    $TARGET')
 	
 	
 	def _GetConfigurationVariables(self, configuration_name):
@@ -174,3 +115,65 @@ class Environment(SCons.Environment.Environment):
 		all_directives.append(conflict_manager_directive)
 
 		return all_directives
+	
+	
+	#: Build options which are either on or off.
+	_boolean_options = {
+		'ROLLOVER_CLOCK':           ('Roll over clock when it reaches its maximum value.  Clock rollover can be safely disabled on 64 bits to save a few cycles, but it is necessary on 32 bits if the application executes more than 2^28 (write-through) or 2^31 (write-back) transactions.',
+			True),
+		'CLOCK_IN_CACHE_LINE':      ('Ensure that the global clock does not share the same cache line than some other variable of the program.  This should be normally enabled.',
+			True),
+		'NO_DUPLICATES_IN_RW_SETS': ('Prevent duplicate entries in read/write sets when accessing the same address multiple times.  Enabling this option may reduce performance so leave it disabled unless transactions repeatedly read or write the same address.',
+			True),
+		'WAIT_YIELD':               ('Yield the processor when waiting for a contended lock to be released. This only applies to the CM_WAIT and CM_PRIORITY contention managers.',
+			True),
+		'EPOCH_GC':                 ('Use an epoch-based memory allocator and garbage collector to ensure that accesses to the dynamic memory allocated by a transaction from another transaction are valid.  There is a slight overhead from enabling this feature.',
+			True),
+		'CONFLICT_TRACKING':        ('Keep track of conflicts between transactions and notifies the application (using a callback), passing the identity of the two conflicting transaction and the associated threads.  This feature requires EPOCH_GC.',
+			True),
+		'READ_LOCKED_DATA':         ('Allow transactions to read the previous version of locked memory locations, as in the original LSA algorithm (see [DISC-06]). This is achieved by peeking into the write set of the transaction that owns the lock.  There is a small overhead with non-contended workloads but it may significantly reduce the abort rate, especially with transactions that read much data.  This feature only works with the WRITE_BACK_ETL design and requires EPOCH_GC.',
+			True),
+		'LOCK_IDX_SWAP':            ('Tweak the hash function that maps addresses to locks so that consecutive addresses do not map to consecutive locks. This can avoid cache line invalidations for application that perform sequential memory accesses. The last byte of the lock index is swapped with the previous byte.',
+			True),
+		
+		# These are more to do with build behavior and output
+		'VERBOSE': ('If set, displays the actual commands used and their flags instead of the default "neat" output.',
+			False)
+	}
+	
+	#: Build options which have enumerated values.
+	_enumerable_options = [
+		EnumVariable('CONFLICT_MANAGER',
+		                 'Determines the conflict_management policy for the STM.',
+		                 'CM_SUICIDE',
+		                 ['CM_SUICIDE', 'CM_DELAY', 'CM_BACKOFF', 'CM_PRIORITY'])
+	]
+	
+	#: Build options which have numerical values
+	_numerical_options = [
+		('RW_SET_SIZE',
+		 'Initial size of the read and write sets. These sets will grow dynamically when they become full.',
+		 4096 # Default
+				 ),
+		('LOCK_ARRAY_LOG_SIZE',
+		 'Number of bits used for indexes in the lock array. The size of the array will be 2 to the power of LOCK_ARRAY_LOG_SIZE.',
+		 20 # Default
+				 ),
+		('MIN_BACKOFF',
+		 'Minimum value of the exponential backoff delay. This parameter is only used with the CM_BACKOFF contention manager.',
+		 0x4 # Default
+		 ),
+		('MAX_BACKOFF',
+		 'Maximum value of the exponential backoff delay. This parameter is only used with the CM_BACKOFF contention manager.',
+		 0x80000000 # Default
+		 ),
+		('VR_THRESHOLD_DEFAULT', 
+		 'Number of aborts due to failed validation before switching to visible reads. A value of 0 indicates no limit. This parameter is only used with the CM_PRIORITY contention manager. It can also be set using an environment variable of the same name.',
+		 3 # Default
+		 ),
+		('CM_THRESHOLD_DEFAULT',
+		 'Number of executions of the transaction with a CM_SUICIDE contention management strategy before switching to CM_PRIORITY. This parameter is only used with the CM_PRIORITY contention manager. It can also be set using an environment variable of the same name.',
+		 0 # Default
+		 )
+		 
+	]

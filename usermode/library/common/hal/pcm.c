98374c99a842301b24641365a5e2e33c9d3cd9d6
Andres Jaan Tack <ajtack@gmail.com>
2010-02-19 07:03:25 -0600

    Resolved some missing symbol link errors.

--- usermode/library/common/hal/pcm.c
+++ usermode/library/common/hal/pcm.c
@@ -11,17 +11,6 @@
 //#define EMULATE_CRASH 0x1
 #undef EMULATE_CRASH
 
-#ifdef __x86_64__
-# define CACHELINE_SIZE 64
-# define CACHELINE_SIZE_LOG 6
-#else
-# define CACHELINE_SIZE 32
-# define CACHELINE_SIZE_LOG 5
-#endif
-
-#define BLOCK_ADDR(addr) ( (pcm_word_t *) ((pcm_word_t) addr & ~(CACHELINE_SIZE - 1)) )
-#define INDEX_ADDR(addr) ( (pcm_word_t *) ((pcm_word_t) addr & (CACHELINE_SIZE - 1)) )
-
 /* Emulate latency */
 #define EMULATE_LATENCY 0x1
 //#undef EMULATE_LATENCY

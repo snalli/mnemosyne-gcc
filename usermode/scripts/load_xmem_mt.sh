23333dc262caf745a70c3577e057b6fe9db7f27f
Sanketh Nalli <nalli@wisc.edu>
2016-12-02 19:30:52 -0600

    memcached fixes

--- usermode/scripts/load_xmem_mt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MEMCACHED_DIR="/u/s/a/sankey/Desktop/mnemosyne/usermode/scripts"
-
-source "$MEMCACHED_DIR/memcached_funcs.sh"
-
-echo "Loading multi-threaded dataset ..."
-load_memcached_dataset 0
-
--- usermode/scripts/run_memslap.sh
+++ usermode/scripts/run_memslap.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-#memslap --cfg_cmd -s 127.0.0.1:11211 -T 1 -c 1 -x 1 -X 64
-#memslap -s 127.0.0.1:11211 -T 1 -c 1 -x 10 -X 64
-memslap -s 127.0.0.1:11211 -T 2 -c 2 -x 10 -X 64
-#memslap -s 127.0.0.1:11211 -T 1 -c 1 -t 1s -X 64
+
+
+MEMCACHED_DIR=$HOME"/Desktop/mnemosyne-gcc/usermode"
+MEMCACHED_RUN_CNF="$MEMCACHED_DIR/scripts/run.cnf"
+
+MEMASLAP_BIN=memaslap
+MEMASLAP_THREADS="4"  
+MEMASLAP_CONCURRENCY="4" 
+
+VALUE_SIZE_ARR=( 128 256 512 )
+SERVER_IP="127.0.0.1"
+SERVER_PORT=11211
+
+idx=0
+for (( idx=0; idx<${#VALUE_SIZE_ARR[@]}; idx++ ))
+do
+	val_size=${VALUE_SIZE_ARR[$idx]}
+	num_ops=5000
+	run_time="1s"
+	#$MEMASLAP_BIN -s $SERVER_IP:$SERVER_PORT -c $MEMASLAP_CONCURRENCY -x $num_ops -T $MEMASLAP_THREADS -X $val_size -F $MEMCACHED_RUN_CNF -d 1
+	$MEMASLAP_BIN -s $SERVER_IP:$SERVER_PORT -c $MEMASLAP_CONCURRENCY -T $MEMASLAP_THREADS -X $val_size -F $MEMCACHED_RUN_CNF -d 1 -t $run_time
+	sleep 1
+done
+
+exit
 

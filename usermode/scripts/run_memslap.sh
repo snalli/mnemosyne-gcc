4ae441af19b389def13e63a1bfc55304001562fb
Sanketh Nalli <nalli@wisc.edu>
2016-12-11 17:39:23 -0600

    saving scripts

--- usermode/scripts/run_memslap.sh
+++ usermode/scripts/run_memslap.sh
@@ -16,10 +16,10 @@ idx=0
 for (( idx=0; idx<${#VALUE_SIZE_ARR[@]}; idx++ ))
 do
 	val_size=${VALUE_SIZE_ARR[$idx]}
-	num_ops=5000
-	run_time="1s"
-	#$MEMASLAP_BIN -s $SERVER_IP:$SERVER_PORT -c $MEMASLAP_CONCURRENCY -x $num_ops -T $MEMASLAP_THREADS -X $val_size -F $MEMCACHED_RUN_CNF -d 1
-	$MEMASLAP_BIN -s $SERVER_IP:$SERVER_PORT -c $MEMASLAP_CONCURRENCY -T $MEMASLAP_THREADS -X $val_size -F $MEMCACHED_RUN_CNF -d 1 -t $run_time
+	num_ops=100000
+	run_time="10s"
+	$MEMASLAP_BIN -s $SERVER_IP:$SERVER_PORT -c $MEMASLAP_CONCURRENCY -x $num_ops -T $MEMASLAP_THREADS -X $val_size -F $MEMCACHED_RUN_CNF -d 1
+	#$MEMASLAP_BIN -s $SERVER_IP:$SERVER_PORT -c $MEMASLAP_CONCURRENCY -T $MEMASLAP_THREADS -X $val_size -F $MEMCACHED_RUN_CNF -d 1 -t $run_time
 	sleep 1
 done
 

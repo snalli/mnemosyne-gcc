86ea136eba503ad5dda939e304673dd18f3b267f
nalli <nalli@wisc.edu>
2017-03-22 21:07:35 -0500

    fix for err : unsafe func call in atomic ctxt

--- usermode/library/mtm/include/tm_def.h
+++ usermode/library/mtm/include/tm_def.h
@@ -1,6 +1,8 @@
 #ifndef __TM_DEF_H__
 #define __TM_DEF_H__
 
+// #include <sys/cdefs.h>
+
 #define TM_SAFE                         __attribute__((transaction_safe))
 #define TM_CALL                         __attribute__((transaction_callable))
 #define TM_PURE                         __attribute__((transaction_pure))
@@ -20,19 +22,19 @@ void __assert_fail (const char *__assertion, const char *__file,
 TM_PURE void *realloc(void *ptr, size_t size);
 TM_PURE long int strtol(const char *nptr, char **endptr, int base);
 TM_PURE unsigned long long int strtoull(const char *nptr, char **endptr, int base);
-/* Test for GCC > 6.2.1 */
-#if __GNUC__ > 6 || \
-	(__GNUC__ == 6 && (__GNUC_MINOR__ > 2 || \
-	(__GNUC_MINOR__ == 2 && \
-	__GNUC_PATCHLEVEL__ > 0)))
-TM_PURE int fprintf (FILE *__restrict __stream, const char *__restrict __fmt, ...);
-TM_PURE int sprintf (char *__restrict __s, const char *__restrict __format, ...) __THROWNL;
-TM_PURE int snprintf (char *__restrict __s, size_t __maxlen, const char *__restrict __format, ...)
-	__THROWNL __attribute__ ((__format__ (__printf__, 3, 4)));
-#endif
 
+/*
+TM_PURE extern int fprintf (FILE *__restrict __stream, const char *__restrict __fmt, ...);
+TM_PURE __fortify_function int sprintf (char *__restrict __s, const char *__restrict __format, ...) __THROWNL;
+TM_PURE __fortify_function int snprintf (char *__restrict __s, size_t __maxlen, const char *__restrict __format, ...)
+	__THROWNL __attribute__ ((__format__ (__printf__, 3, 4)));
+TM_PURE __fortify_function int vsprintf (char *__restrict __s, const char *__restrict __fmt, _G_va_list __ap);
+TM_PURE __fortify_function int vsnprintf (char *__restrict __s, size_t __n, const char *__restrict __fmt, _G_va_list __ap);
+*/
 
 TM_PURE size_t __builtin_object_size (void * ptr, int type);
+TM_PURE int __fprintf_chk (FILE *__restrict __stream, int __flag,
+                          const char *__restrict __format, ...);
 TM_PURE int __builtin___sprintf_chk (char *s, int flag, size_t os, const char *fmt, ...);
 TM_PURE int __builtin___snprintf_chk (char *s, size_t maxlen, int flag, size_t os, const char *fmt, ...);
 TM_PURE int __builtin___vsprintf_chk (char *s, int flag, size_t os, const char *fmt, va_list ap);

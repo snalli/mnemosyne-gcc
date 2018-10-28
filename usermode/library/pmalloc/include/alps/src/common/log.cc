06cdb189a8cca6bad5eee16ccaf9c849fc36bcbe
Swapnil Haria <swapnilster@gmail.com>
2018-10-28 18:10:24 -0500

    Adding fix for boost empty_deleter

--- usermode/library/pmalloc/include/alps/src/common/log.cc
+++ usermode/library/pmalloc/include/alps/src/common/log.cc
@@ -26,7 +26,7 @@
 #include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/utility/empty_deleter.hpp>
+#include <boost/utility/empty_deleter.hpp>
 // #include <boost/utility/empty_deleter.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/shared_ptr.hpp>
@@ -64,7 +64,7 @@ void init_log(const DebugOptions& debug_options)
 
     // add "console" output stream to our sink
     // we have to provide an empty deleter to avoid destroying the global stream object
-    boost::shared_ptr< std::ostream > stream(&std::clog, boost::log::v2_mt_posix::empty_deleter());
+    boost::shared_ptr< std::ostream > stream(&std::clog, boost::empty_deleter());
     sink->locked_backend()->add_stream(stream);
 
     // specify the format of the log message
